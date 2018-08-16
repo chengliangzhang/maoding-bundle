3c4a6b6eb6256ee0c36b0968c7610a7db9052be3
zhangchengliang <zhangchengliang@imaoding.com>
2018-08-16 21:39:37 +0800

    完成数据库读写，但无法运行测试

---
+++ basic-core/src/test/java/com/maoding/mybatis/TestEntity.java
@@ -0,0 +1,16 @@
+package com.maoding.mybatis;
+
+import com.maoding.core.base.BaseEntity;
+
+import javax.persistence.Table;
+
+/**
+ * 深圳市卯丁技术有限公司
+ * 日期: 2018/8/16
+ * 类名: com.maoding.mybatis.provider.TestDTO
+ * 作者: 张成亮
+ * 描述:
+ **/
+@Table(name = "md_list_storage_file")
+public class TestEntity extends BaseEntity {
+}
