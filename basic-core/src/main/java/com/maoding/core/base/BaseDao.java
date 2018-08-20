a05177aac36e21530be504aadbd554bd1e45cde2
zhangchengliang <zhangchengliang@imaoding.com>
2018-08-20 10:38:20 +0800

    添加getPathField,getPathSplit，合并update和updateById

--- basic-core/src/main/java/com/maoding/core/base/BaseDao.java
+++ basic-core/src/main/java/com/maoding/core/base/BaseDao.java
@@ -16,41 +16,34 @@ public interface BaseDao<T extends BaseEntity> extends BaseViewDao<T>,Mapper<T>,
     //FIXME 特别注意，该接口不能被扫描到，否则会出错
 
     /**
-     * 描述       更新记录
+     * 描述       通过更新申请进行记录更新
      * 日期       2018/8/16
      * @author   张成亮
+     * @param    request 更新申请
+     *                   accountId 申请者用户编号
+     * @return   返回更新后的记录
      **/
-    default void updateById(@NotNull CoreEditDTO request){
+    default T updateById(@NotNull CoreEditDTO request){
         Class<T> clazz = getT();
         T entity = BeanUtils.createFrom(request,clazz);
-        updateByPrimaryKeySelective(entity);
+        entity.setUpdateBy(request.getAccountId());
+        return updateById(entity);
     }
 
+    /**
+     * 描述       通过更新申请进行记录更新，需要判断记录是否被选中更新
+     * 日期       2018/8/16
+     * @author   张成亮
+     * @param    request 更新申请
+     *                   accountId 申请者用户编号
+     *                   isSelected 是否被选中进行更新
+     * @return   如果选中进行更新，返回更新后的记录，否则返回空
+     **/
     default T update(@NotNull CoreEditDTO request){
         final String unselected = "0";
         T entity = null;
         if (StringUtils.isNotSame(request.getIsSelected(), unselected)) {
-            //如果request内的id不为空,则从数据库内读取，如果为空，则新增，如果不为空，则更改
-            if (StringUtils.isNotEmpty(request.getId())){
-                entity = selectById(request.getId());
-                if (entity != null) {
-                    //修改
-                    BeanUtils.copyProperties(request, entity);
-                    entity.setUpdateBy(request.getAccountId());
-                    entity.resetUpdateDate();
-                    updateByPrimaryKeySelective(entity);
-                }
-            }
-
-            //如果request的id为空，或者数据库内没有此记录，则新增记录
-            if (entity == null) {
-                //如果id为空，则新增一条记录
-                Class<T> clazz = getT();
-                entity = BeanUtils.createFrom(request, clazz);
-                entity.initEntity();
-                entity.setCreateBy(request.getAccountId());
-                insertSelective(entity);
-            }
+            entity = updateById(request);
         }
         return entity;
     }
@@ -71,10 +64,18 @@ public interface BaseDao<T extends BaseEntity> extends BaseViewDao<T>,Mapper<T>,
         //如果entity的id为空，或者数据库内没有此记录，则新增记录
         if (entity == null) {
             entity = request;
-            entity.initEntity();
-            insertSelective(entity);
+            insert(entity);
         }
         return entity;
+    }
+
+    default  int insert(@NotNull T entity){
+        if (StringUtils.isEmpty(entity.getId())){
+            entity.initEntity();
+        } else if (entity.getCreateDate() == null) {
+            entity.resetCreateDate();
+        }
+        return insertSelective(entity);
     }
 
     default int BatchInsert(List<T> recordList){
--- basic-core/src/main/java/com/maoding/core/base/BaseIdObject.java
+++ basic-core/src/main/java/com/maoding/core/base/BaseIdObject.java
@@ -1,6 +1,10 @@
 package com.maoding.core.base;
 
+import com.maoding.utils.BeanUtils;
+import com.maoding.utils.StringUtils;
+
 import javax.persistence.Id;
+import javax.persistence.Transient;
 import java.io.Serializable;
 
 /**
@@ -15,11 +19,43 @@ public class BaseIdObject implements Serializable,Cloneable {
     @Id
     private String id;
 
+    /** 路径字段名,用于更新保存树结构数据的表 **/
+    @Transient
+    private String pathField;
+
+    /** 路径内分隔字符串，用于保存树结构数据内树路径，原始表结构内通常为"-"，新表接口内通常为"/" **/
+    @Transient
+    private String pathSplit;
+
     public String getId() {
         return id;
     }
 
     public void setId(String id) {
         this.id = id;
+    }
+
+    /**
+     * 判断是否存在路径字段，及路径字段是否为空
+     **/
+    public boolean isPathEmpty(){
+        return StringUtils.isEmpty(getPathField())
+                || StringUtils.isEmpty(BeanUtils.getProperty(this,getPathField(),String.class,true));
+    }
+
+    public void setPathField(String pathField) {
+        this.pathField = pathField;
+    }
+
+    public String getPathField() {
+        return pathField;
+    }
+
+    public void setPathSplit(String pathSplit) {
+        this.pathSplit = pathSplit;
+    }
+
+    public String getPathSplit() {
+        return pathSplit;
     }
 }
