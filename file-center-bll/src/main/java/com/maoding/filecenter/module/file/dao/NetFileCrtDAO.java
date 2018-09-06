e294da61ca7ce7284ee758255fbf7498cf413967
zhangchengliang <zhangchengliang@imaoding.com>
2018-09-06 16:52:37 +0800

    修正@Autowire存在波浪线问题

--- file-center-bll/src/main/java/com/maoding/filecenter/module/file/dao/NetFileCrtDAO.java
+++ file-center-bll/src/main/java/com/maoding/filecenter/module/file/dao/NetFileCrtDAO.java
@@ -1,15 +1,13 @@
 package com.maoding.filecenter.module.file.dao;
 
 import com.maoding.core.base.BaseDao;
-import com.maoding.filecenter.module.file.dto.DirectoryDTO;
 import com.maoding.filecenter.module.file.model.NetFileCrtDO;
-import com.maoding.filecenter.module.file.model.NetFileDO;
-
-import java.util.List;
+import org.springframework.stereotype.Repository;
 
 /**
  * Created by Wuwq on 2017/05/31.
  */
+@Repository
 public interface NetFileCrtDAO extends BaseDao<NetFileCrtDO> {
 
 }
--- file-center-bll/src/main/java/com/maoding/filecenter/module/im/dao/ImGroupDAO.java
+++ file-center-bll/src/main/java/com/maoding/filecenter/module/im/dao/ImGroupDAO.java
@@ -1,10 +1,12 @@
 package com.maoding.filecenter.module.im.dao;
 
 import com.maoding.filecenter.module.im.dto.GroupImgUpdateDTO;
+import org.springframework.stereotype.Repository;
 
 /**
  * Created by Wuwq on 2017/05/31.
  */
+@Repository
 public interface ImGroupDAO {
     int updateGroupImg(GroupImgUpdateDTO dto);
 }
